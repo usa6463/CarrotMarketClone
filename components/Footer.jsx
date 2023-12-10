0986e47f99895a64315bf0a39875e0908b6481ae
usa6463 <usa6463@gmail.com>
2023-12-10 17:17:26 +0900

    Add Button Image to Footer
    - icon 참조: https://thenounproject.com/

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,22 +1,27 @@
-import {TouchableOpacity, View, Text, StyleSheet} from "react-native";
+import {TouchableOpacity, View, Text, StyleSheet, Image} from "react-native";
 
 export default function Footer() {
     return (
         <View style={styles.container}>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Button 1</Text>
+                <Image source={require('../assets/home.png')} style={styles.mapMarkerImage} />
+                <Text style={styles.buttonText}>홈</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Button 2</Text>
+                <Image source={require('../assets/docs.png')} style={styles.mapMarkerImage} />
+                <Text style={styles.buttonText}>동네생활</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Button 3</Text>
+                <Image source={require('../assets/map.png')} style={styles.mapMarkerImage} />
+                <Text style={styles.buttonText}>내 근처</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Button 4</Text>
+                <Image source={require('../assets/chat.png')} style={styles.mapMarkerImage} />
+                <Text style={styles.buttonText}>채팅</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Button 5</Text>
+                <Image source={require('../assets/profile.png')} style={styles.mapMarkerImage} />
+                <Text style={styles.buttonText}>나의 당근</Text>
             </TouchableOpacity>
         </View>
     );
@@ -27,12 +32,17 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
-        backgroundColor: '#e0e0e0',
+        backgroundColor: '#212123',
         paddingVertical: 10,
         position: 'absolute',
         bottom: 0,
         left: 0,
         right: 0,
+    },
+    mapMarkerImage: {
+        width: 20, // 이미지의 폭 조절
+        height: 20, // 이미지의 높이 조절
+        marginRight: 10, // 이미지와 텍스트 사이의 간격 조절
     },
     button: {
         backgroundColor: '#3498db',
