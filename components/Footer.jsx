1bb97abd694835ce9addee411e77940893aa1676
usa6463 <usa6463@gmail.com>
2023-12-11 21:23:50 +0900

    Extract variable from svg properties

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,26 +6,29 @@ import SearchLocation from '../assets/search-location.svg';
 import Messages from '../assets/messages.svg';
 
 export default function Footer({onButtonClick}) {
+    const svg_default_width = 50
+    const svg_default_height = 20
+    const svg_default_line_color = "#fff"
     return (
         <View style={styles.container}>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("home")}>
-                <HouseChimney width={50} height={20} fill={"#fff"}></HouseChimney>
+                <HouseChimney width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></HouseChimney>
                 <Text style={styles.buttonText}>홈</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("townlife")}>
-                <Memo width={60} height={20} fill={"#fff"}></Memo>
+                <Memo width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></Memo>
                 <Text style={styles.buttonText}>동네생활</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("near")}>
-                <SearchLocation width={60} height={20} fill={"#fff"}></SearchLocation>
+                <SearchLocation width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></SearchLocation>
                 <Text style={styles.buttonText}>내 근처</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("chat")}>
-                <Messages width={60} height={20} fill={"#fff"}></Messages>
+                <Messages width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></Messages>
                 <Text style={styles.buttonText}>채팅</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("mycarrot")}>
-                <CircleUser width={60} height={20} fill={"#fff"}></CircleUser>
+                <CircleUser width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></CircleUser>
                 <Text style={styles.buttonText}>나의 당근</Text>
             </TouchableOpacity>
         </View>
