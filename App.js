a0c5bede4afaaced0d7ba3651ed007e45c862b9f
usa6463 <usa6463@gmail.com>
2023-12-11 20:44:54 +0900

    Add Main page state

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Footer from "./components/Footer";
 
 export default function App() {
+  const [currentMainPage, setCurrentMainPage] = useState("home")
+
+  const onButtonClick = (componentName) => {
+      setCurrentMainPage(componentName)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Carrot Market Clone Coding App</Text>
+      <Text>Carrot Market Clone Coding App : {currentMainPage} </Text>
       <StatusBar style="auto" />
-      <Footer/>
+      <Footer onButtonClick={ onButtonClick }/>
     </View>
   );
 }
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,26 +5,26 @@ import Memo from '../assets/memo.svg';
 import SearchLocation from '../assets/search-location.svg';
 import Messages from '../assets/messages.svg';
 
-export default function Footer() {
+export default function Footer({ onButtonClick }) {
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={ () => onButtonClick("home") }>
                 <HouseChimney width={50} height={20} fill={"#fff"}></HouseChimney>
                 <Text style={styles.buttonText}>홈</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={ () => onButtonClick("townlife") }>
                 <Memo width={60} height={20} fill={"#fff"}></Memo>
                 <Text style={styles.buttonText}>동네생활</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={ () => onButtonClick("near") }>
                 <SearchLocation width={60} height={20} fill={"#fff"}></SearchLocation>
                 <Text style={styles.buttonText}>내 근처</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={ () => onButtonClick("chat") }>
                 <Messages width={60} height={20} fill={"#fff"}></Messages>
                 <Text style={styles.buttonText}>채팅</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={ () => onButtonClick("mycarrot") }>
                 <CircleUser width={60} height={20} fill={"#fff"}></CircleUser>
                 <Text style={styles.buttonText}>나의 당근</Text>
             </TouchableOpacity>
