627fb52775f97de871172f7b3e26141eaeb93512
usa6463 <usa6463@gmail.com>
2023-12-12 20:17:29 +0900

    Update button hover to others

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,28 +13,24 @@ export default function Footer({ onButtonClick, currentMainPage }) {
     return (
         <View style={styles.container}>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("home")}>
-                {currentMainPage === "home" ?
-                    (<HouseChimney width={svg_default_width} height={svg_default_height} fill={svg_active_line_color}></HouseChimney>) :
-                    (<HouseChimney width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></HouseChimney>)}
-                {currentMainPage === "home" ?
-                    (<Text style={styles.activeButtonText}>홈</Text>) :
-                    (<Text style={styles.buttonText}>홈</Text>)}
+                <HouseChimney width={svg_default_width} height={svg_default_height} fill={currentMainPage === "home" ? svg_active_line_color:svg_default_line_color}></HouseChimney>
+                <Text style={currentMainPage === "home" ? styles.activeButtonText:styles.buttonText}>홈</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("townlife")}>
-                <Memo width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></Memo>
-                <Text style={styles.buttonText}>동네생활</Text>
+                <Memo width={svg_default_width} height={svg_default_height} fill={currentMainPage === "townlife" ? svg_active_line_color:svg_default_line_color}></Memo>
+                <Text style={currentMainPage === "townlife" ? styles.activeButtonText:styles.buttonText}>동네생활</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("near")}>
-                <SearchLocation width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></SearchLocation>
-                <Text style={styles.buttonText}>내 근처</Text>
+                <SearchLocation width={svg_default_width} height={svg_default_height} fill={currentMainPage === "near" ? svg_active_line_color:svg_default_line_color}></SearchLocation>
+                <Text style={currentMainPage === "near" ? styles.activeButtonText:styles.buttonText}>내 근처</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("chat")}>
-                <Messages width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></Messages>
-                <Text style={styles.buttonText}>채팅</Text>
+                <Messages width={svg_default_width} height={svg_default_height} fill={currentMainPage === "chat" ? svg_active_line_color:svg_default_line_color}></Messages>
+                <Text style={currentMainPage === "chat" ? styles.activeButtonText:styles.buttonText}>채팅</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => onButtonClick("mycarrot")}>
-                <CircleUser width={svg_default_width} height={svg_default_height} fill={svg_default_line_color}></CircleUser>
-                <Text style={styles.buttonText}>나의 당근</Text>
+                <CircleUser width={svg_default_width} height={svg_default_height} fill={currentMainPage === "mycarrot" ? svg_active_line_color:svg_default_line_color}></CircleUser>
+                <Text style={currentMainPage === "mycarrot" ? styles.activeButtonText:styles.buttonText}>나의 당근</Text>
             </TouchableOpacity>
         </View>
     );
