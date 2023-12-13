fa187eabebbceeb43d2d84b18d8fc7a5ed49f811
usa6463 <usa6463@gmail.com>
2023-12-13 21:58:17 +0900

    Update Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,18 +1,29 @@
-import {StyleSheet, Text, View} from "react-native";
+import {ScrollView, StyleSheet, Text, View} from "react-native";
 
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>Home Component!</Text>
+            <Text>지역명</Text>
+            <ScrollView>
+                <Text>포르쉐 911</Text>
+                <Text>갤럭시 제트폴드3</Text>
+            </ScrollView>
         </View>
+
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
+        flex:1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
         alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#212123',
+        paddingVertical: 0,
+        // position: 'absolute',
+        // bottom: 0,
+        // left: 0,
+        // right: 0,
     },
 });
