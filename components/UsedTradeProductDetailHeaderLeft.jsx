a038e163eb86cd59551defdf1f5c2a0a3da2d260
usa6463 <usa6463@gmail.com>
2023-12-25 10:45:21 +0900

    Add UsedTradeProductDetailHeaderLeft left button onPress

--- components/UsedTradeProductDetailHeaderLeft.jsx
+++ components/UsedTradeProductDetailHeaderLeft.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import AngleLeft from '../assets/component-icon/angle-left.svg';
 import HouseChimney from '../assets/component-icon/house-chimney.svg';
+import { useNavigation } from '@react-navigation/native';
 
 export default function UsedTradeProductDetailHeaderLeft() {
+    const navigation = useNavigation();
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={{marginRight: 25}}>
+            <TouchableOpacity style={{marginRight: 25}} onPress={()=> {navigation.goBack()}}>
                 <AngleLeft width="20" height="20" fill="white"></AngleLeft>
             </TouchableOpacity>
             <TouchableOpacity>
