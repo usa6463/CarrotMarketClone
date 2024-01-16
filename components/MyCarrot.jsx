820e835e80125a632b32c1d129c2e0b98eb64798
usa6463 <usa6463@gmail.com>
2024-01-17 06:56:05 +0900

    Add react-native-modal test code to MyCarrot temporarily

--- components/MyCarrot.jsx
+++ components/MyCarrot.jsx
@@ -1,9 +1,28 @@
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, Button} from "react-native";
+import Modal from "react-native-modal";
+import React, { useState } from "react";
 
 export default function MyCarrot() {
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    };
+
     return (
         <View style={styles.container}>
-            <Text>MyCarrot Component!</Text>
+            <Button title="Show modal" onPress={toggleModal} />
+
+            <Modal
+                isVisible={isModalVisible}
+                animationIn={"lightSpeedIn"}
+            >
+                <View style={{ flex: 1 }}>
+                    <Text style={{'color':'white'}}>Hello!</Text>
+
+                    <Button title="Hide modal" onPress={toggleModal} />
+                </View>
+            </Modal>
         </View>
     )
 }
